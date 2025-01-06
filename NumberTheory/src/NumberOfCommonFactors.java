726602a1e9343110057daa6fa081a05501a7f017
msvas999 <srinivasmallabathul@gmail.com>
2025-01-06 08:49:01 +0530

    user gcd

--- NumberTheory/src/NumberOfCommonFactors.java
+++ NumberTheory/src/NumberOfCommonFactors.java
@@ -1,15 +1,33 @@
-import static java.lang.Math.min;
+import java.math.BigInteger;
+
+import static java.lang.Math.sqrt;
 
 public class NumberOfCommonFactors {
     public static void main(String a[]){
-        int x = 4;
-        int y = 8;
-        for(int i=2;i<=min(x,y);i++){
+        BigInteger x = new BigInteger(a[0]);
+        BigInteger y = new BigInteger(a[1]);
+        /*int cnt = 0;
+        for(int i=1;i<=min(x,y);i++){
             if (x % i == 0 && y % i == 0) {
                 System.out.println(i);
+                cnt++;
             }
 
         }
+        System.out.println("Total number of factors are: "+cnt);*/
+        int gcd_value = x.gcd(y).intValue();
+        int n = gcd_value;
+        System.out.println("GCD: "+gcd_value);
+        int cnt = 0;
+        for (int i=1;i<=sqrt(n);i++){
+            if(n % i == 0){
+                cnt++;
+                if (n / i != i)
+                    cnt ++;
+            }
+
+        }
+        System.out.println("Number of Common Factors are:  "+cnt);
 
     }
 }
